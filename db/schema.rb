875dd89e4c4f8db19e65586bbf2a6f9cb9e52f4e
kamoshibito510 <kamoshibito510@icloud.com>
2019-12-03 01:00:51 +0900

    gem "payjp" を入れたりした。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_081030) do
+ActiveRecord::Schema.define(version: 2019_12_02_154041) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "state", null: false
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2019_11_11_081030) do
     t.datetime "updated_at", null: false
     t.index ["sellitem_id"], name: "index_buyitems_on_sellitem_id"
     t.index ["user_id"], name: "index_buyitems_on_user_id"
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -86,7 +94,7 @@ ActiveRecord::Schema.define(version: 2019_11_11_081030) do
     t.date "birth", null: false
     t.integer "phone_number", null: false
     t.string "comment", null: false
-    t.integer "good", null: false
+    t.timestamp "good", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.string "sellitem", null: false
     t.string "buyitem", null: false
     t.datetime "created_at", null: false
