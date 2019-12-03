5baa04eff54cfed749baffcba702412e9691b807
kamoshibito510 <kamoshibito510@icloud.com>
2019-12-04 01:41:34 +0900

    s

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,26 @@
 Rails.application.routes.draw do
-  get 'card/new'
-  get 'card/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "products#index"
   
-  get 'mypage', to: 'mypages#index'
 
-  get 'index', to: 'users#index'
-  resource :products
-  resource :users
-  get 'users', to: 'users#login-new'
+  root to: 'users#index'
+  get 'users/index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  # root to: "products#index"
+  # get 'mypage', to: 'mypages#index'
+  # get 'profile', to: 'mypages#edit' #仮ルーティング、プロフィール編集画面
+  # get 'identification', to: 'mypages#identification' #仮ルーティング、ユーザー本人確認画面
+  get 'users', to: 'users#new' 
+  # get 'login', to: 'users#login-new' #仮ルーティング、ログイン画面用のビュー
+  # get 'logout', to: 'users#logout' #仮ルーティング、ログアウト画面用のビュー
+  # get 'buy', to: 'users#buy' #仮ルーティング、商品購入確認ページ
+  # get 'card', to: 'users#card' #仮ルーティング、クレジットカード登録ページ
+  # get 'sell', to: 'products#sell' #仮ルーティング、商品出品ページ
+  # get 'detail', to: 'products#detail' #仮ルーティング、商品詳細ページ
+  
+  
+  # resource :products
+  # resource :users
 
 end
