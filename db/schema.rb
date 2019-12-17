387b5478c3fb05e96716402834fd112b7ae3aeb2
kamoshibito510 <kamoshibito510@icloud.com>
2019-12-17 20:17:20 +0900

    Cardテーブル、コントローラー、ビューの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_123005) do
+ActiveRecord::Schema.define(version: 2019_12_17_111059) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "state", null: false
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2019_12_09_123005) do
     t.datetime "updated_at", null: false
     t.index ["sellitem_id"], name: "index_buyitems_on_sellitem_id"
     t.index ["user_id"], name: "index_buyitems_on_user_id"
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "customer_id"
+    t.string "card_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
